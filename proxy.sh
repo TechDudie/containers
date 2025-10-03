9a332da2a96c16308db895ec1a2efe49691889af
TechnoDot <73961295+TechDudie@users.noreply.github.com>
2025-10-02 22:58:52 -0500

    Update proxy.sh

--- proxy.sh
+++ proxy.sh
@@ -1,33 +1,25 @@
 sudo apt update
 sudo apt install microsocks ufw -y
-echo Allowing 22/tcp
 sudo ufw allow 22/tcp
-echo Allowing 22/udp
-sudo ufw allow 22/udp
-echo Allowing 1080/tcp
-sudo ufw allow 1080/tcp
-echo Allowing 1080/udp
-sudo ufw allow 1080/udp
-echo Enabling ufw service
+sudo ufw allow 24167/tcp
 sudo ufw --force enable
 
 echo Creating system service /etc/systemd/system/proxy.service
 sudo tee -a /etc/systemd/system/proxy.service > /dev/null <<EOF
 [Unit]
-Description=microsocks socks5://0.0.0.0:1080
+Description=microsocks socks5://0.0.0.0:24167
 Documentation=https://github.com/rofl0r/microsocks
 After=network.target
 
 [Service]
 Type=simple
 Restart=always
-ExecStart=microsocks
+ExecStart=microsocks -p 24167
 
 [Install]
 WantedBy=multi-user.target
 EOF
 
-echo Enabling system service
 systemctl start proxy.service
 sudo systemctl enable proxy.service
 echo Done!
